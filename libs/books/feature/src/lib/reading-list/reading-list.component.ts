1418628270fce3939cbe72c81000db7d9f97e8bc
aditya13jani <44555223+aditya13jani@users.noreply.github.com>
2020-06-02 13:39:09 -0400

    task:3 Undo add and remove from reading list

--- libs/books/feature/src/lib/reading-list/reading-list.component.ts
+++ libs/books/feature/src/lib/reading-list/reading-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { getReadingList, removeFromReadingList } from '@tmo/books/data-access';
+import { getReadingList, removeFromReadingList, failedRemoveFromReadingList } from '@tmo/books/data-access';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'tmo-reading-list',
@@ -10,9 +11,20 @@ import { getReadingList, removeFromReadingList } from '@tmo/books/data-access';
 export class ReadingListComponent {
   readingList$ = this.store.select(getReadingList);
 
-  constructor(private readonly store: Store) {}
+  constructor(
+    private readonly store: Store,
+    private _snackBar: MatSnackBar  
+  ) {}
 
   removeFromReadingList(item) {
     this.store.dispatch(removeFromReadingList({ item }));
+    console.log(':::',item);
+    const snackBarRef = this._snackBar.open(`${item.title} is removed from My Reading List`, 'Undo', {
+      duration: 2000,
+    });
+    snackBarRef.onAction().subscribe(() => {
+      this.store.dispatch(failedRemoveFromReadingList({ item }));
+      console.log('The snack-bar action was triggered!::::', item);
+    });
   }
 }
